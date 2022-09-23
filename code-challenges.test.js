2c4dc67d97239d7edde622f595d41876cd674e1b
JeremyDuncan <jeremy.duncan1984@gmail.com>
2022-09-23 13:51:43 -0700

    completed first challenge. All tests pass.

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -49,7 +49,7 @@ describe("getFibonacci", () => {
   });
 });
 
-//----------------------------Initial Tests ------------------------------------
+//---------------------|| Initial Test Results ||-------------------------------
 // ● getFibonacci › returns that number must be great then 2 if negative number
 
 // ReferenceError: getFibonacci is not defined
@@ -72,8 +72,64 @@ describe("getFibonacci", () => {
 // error Command failed with exit code 1.
 // info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this
 // command.
+//------------------------------------------------------------------------------
 
+//------------------------------------------------------------------------------
 // b) Create the function that makes the test pass.
+//------------------------------------------------------------------------------
+
+// declare function
+const getFibonacci = (num) => {
+  // declare variables to manipulate fibanacci sequence with.
+  // These are stored outside the scope of the loop so that the values will be
+  // persistent across iterations.
+  let a = 0;
+  let b = 1;
+  let c;
+
+  // create empty array to hold numbers
+  let fibArr = [];
+
+  // if the input number is less than or equal 2 then return message
+  if (num <= 2) {
+    return "Number must be greater than 2";
+  }
+
+  // Create for-loop to cycle through iterations based on the input number.
+  // add the value of B into the Array which should be initally 1..
+  // then set the inital value of C to equal the sum of A & B..
+  // then assign the value of B to A..
+  // Then Assign the value of C to B..
+  // Repeat loop which will increment the fibanacci sequence until the
+  // iterations based on the input number is complete.
+  for (let i = 0; i < num; i++) {
+    fibArr.push(b);
+    c = a + b;
+    a = b;
+    b = c;
+  }
+
+  // Return the result of the fibonacci sequence.
+  return fibArr;
+};
+
+//--------------------------|| Final Test Results ||----------------------------
+// yarn run v1.22.19
+// warning package.json: No license field
+// $ /Users/learnacademy/Desktop/Repos/week-3-assessment-JeremyDuncan/
+// node_modules/.bin/jest
+//  PASS  ./code-challenges.test.js
+//   getFibonacci
+//     ✓ returns an array of numbers in Fibonacci sequence (1 ms)
+//     ✓ returns that number must be great then 2
+//     ✓ returns that number must be great then 2 if negative number
+
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
+// Snapshots:   0 total
+// Time:        0.18 s, estimated 1 s
+// Ran all test suites.
+// ✨  Done in 0.72s.
 //------------------------------------------------------------------------------
 
 // --------------------2) Create a function that takes in an object and returns
