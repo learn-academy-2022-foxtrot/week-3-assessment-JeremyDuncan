6b1c001be648902daf872794e248c4ac30acdb2a
JeremyDuncan <jeremy.duncan1984@gmail.com>
2022-09-23 13:36:20 -0700

    created test cases for challenge #1

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -11,19 +11,70 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
-// --------------------1) Create a function that takes in a number (greater
-// than 2) and returns an array that length containing the numbers of the
-// Fibonacci sequence.
-
+//==============================================================================
+// --------------------1) Create a function that takes in a number
+// (greater than 2) and returns an array that length containing the numbers
+// of the Fibonacci sequence.
+//==============================================================================
 // a) Create a test with expect statements for each of the variables provided.
+//------------------------------------------------------------------------------
 
-const fibLength1 = 6;
-// Expected output: [1, 1, 2, 3, 5, 8]
+describe("getFibonacci", () => {
+  it("returns an array of numbers in Fibonacci sequence", () => {
+    //=== Provided Test Cases ===
+    const fibLength1 = 6;
+    const fibLength2 = 10;
+    //=== Expected Test Results ===
+    const expected1 = [1, 1, 2, 3, 5, 8];
+    const expected2 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
 
-const fibLength2 = 10;
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    expect(getFibonacci(fibLength1)).toEqual(expected1);
+    expect(getFibonacci(fibLength2)).toEqual(expected2);
+  });
+  it("returns that number must be great then 2", () => {
+    //=== Additional Edge Case ===
+    const fibLength3 = 2;
+    //=== Expected Test Results ===
+    const expected3 = "Number must be greater than 2";
+
+    expect(getFibonacci(fibLength3)).toEqual(expected3);
+  });
+  it("returns that number must be great then 2 if negative number", () => {
+    //=== Additional Edge Case ===
+    const fibLength4 = -22;
+    //=== Expected Test Results ===
+    const expected4 = "Number must be greater than 2";
+
+    expect(getFibonacci(fibLength4)).toEqual(expected4);
+  });
+});
+
+//----------------------------Initial Tests ------------------------------------
+// ● getFibonacci › returns that number must be great then 2 if negative number
+
+// ReferenceError: getFibonacci is not defined
+
+//   46 |     const expected4 = "Number must be greater than 2";
+//   47 |
+// > 48 |     expect(getFibonacci(fibLength4)).toEqual(expected4);
+//      |     ^
+//   49 |   });
+//   50 | });
+//   51 |
+
+//   at Object.expect (code-challenges.test.js:48:5)
+
+// Test Suites: 1 failed, 1 total
+// Tests:       3 failed, 3 total
+// Snapshots:   0 total
+// Time:        0.245 s
+// Ran all test suites.
+// error Command failed with exit code 1.
+// info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this
+// command.
 
 // b) Create the function that makes the test pass.
+//------------------------------------------------------------------------------
 
 // --------------------2) Create a function that takes in an object and returns
 // an array of the numbers sorted from least to greatest.
