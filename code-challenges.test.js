75e65eaa9e4660634ec94c7694cd159b671df033
Jeremy Duncan <jduncan46@my.devry.edu>
2022-09-24 22:08:39 -0400

    initial commit

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,7 +1,5 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
-const { clear } = require("console");
-
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -12,275 +10,23 @@ const { clear } = require("console");
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-//==============================================================================
-// --------------------1)👉 Create a function that takes in a number
-// (greater than 2) and returns an array that length containing the numbers
-// of the Fibonacci sequence.
-//==============================================================================
-// a)🧪 Create a test with expect statements for each of the variables provided.
-//------------------------------------------------------------------------------
-=======
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
->>>>>>> parent of 3c30ea8 (updated formatting)
-=======
-// --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
->>>>>>> parent of 3c30ea8 (updated formatting)
-=======
-// --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
->>>>>>> parent of 3c30ea8 (updated formatting)
-=======
-// --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
->>>>>>> parent of 3c30ea8 (updated formatting)
 
-describe("getFibonacci", () => {
-  it("returns an array of numbers in Fibonacci sequence", () => {
-    // === Provided Test Cases ===
-    const fibLength1 = 6;
-    const fibLength2 = 10;
-    //=== Expected Test Results ===
-    const expected1 = [1, 1, 2, 3, 5, 8];
-    const expected2 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+// a) Create a test with expect statements for each of the variables provided.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    expect(getFibonacci(fibLength1)).toEqual(expected1);
-    expect(getFibonacci(fibLength2)).toEqual(expected2);
-  });
-  it("returns that number must be greater than 2", () => {
-    //=== Additional Edge Case ===
-    const fibLength3 = 2;
-    //=== Expected Test Results ===
-    const expected3 = "Number must be greater than 2";
-
-    expect(getFibonacci(fibLength3)).toEqual(expected3);
-  });
-  it("returns that number must be greater than 2 if negative number", () => {
-    //=== Additional Edge Case ===
-    const fibLength4 = -22;
-    //=== Expected Test Results ===
-    const expected4 = "Number must be greater than 2";
-=======
-=======
->>>>>>> parent of 3c30ea8 (updated formatting)
-=======
->>>>>>> parent of 3c30ea8 (updated formatting)
-=======
->>>>>>> parent of 3c30ea8 (updated formatting)
 const fibLength1 = 6
 // Expected output: [1, 1, 2, 3, 5, 8]
 
 const fibLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
->>>>>>> parent of 3c30ea8 (updated formatting)
 
-    expect(getFibonacci(fibLength4)).toEqual(expected4);
-  });
-});
+// b) Create the function that makes the test pass.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-//---------------------||❌ Initial Test Results ❌||---------------------------
-// ● getFibonacci › returns that number must be greater than 2 if negative number
-=======
 // --------------------2) Create a function that takes in an object and returns an array of the numbers sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
->>>>>>> parent of 3c30ea8 (updated formatting)
-=======
-// --------------------2) Create a function that takes in an object and returns an array of the numbers sorted from least to greatest.
-// Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
->>>>>>> parent of 3c30ea8 (updated formatting)
-=======
-// --------------------2) Create a function that takes in an object and returns an array of the numbers sorted from least to greatest.
-// Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
->>>>>>> parent of 3c30ea8 (updated formatting)
-=======
-// --------------------2) Create a function that takes in an object and returns an array of the numbers sorted from least to greatest.
-// Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
->>>>>>> parent of 3c30ea8 (updated formatting)
 
-// ReferenceError: getFibonacci is not defined
+// a) Create a test with expect statements for each of the variables provided.
 
-<<<<<<< HEAD
-//   46 |     const expected4 = "Number must be greater than 2";
-//   47 |
-// > 48 |     expect(getFibonacci(fibLength4)).toEqual(expected4);
-//      |     ^
-//   49 |   });
-//   50 | });
-//   51 |
-
-//   at Object.expect (code-challenges.test.js:48:5)
-
-// Test Suites: 1 failed, 1 total
-// Tests:       3 failed, 3 total
-// Snapshots:   0 total
-// Time:        0.245 s
-// Ran all test suites.
-// error Command failed with exit code 1.
-// info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this
-// command.
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// b)👨‍💻 Create the function that makes the test pass.
-//------------------------------------------------------------------------------
-
-// declare function
-const getFibonacci = (num) => {
-  // declare variables to manipulate fibanacci sequence with.
-  // These are stored outside the scope of the loop so that the values will be
-  // persistent across iterations.
-  let a = 0;
-  let b = 1;
-  let c;
-
-  // create empty array to hold numbers
-  let fibArr = [];
-
-  // if the input number is less than or equal 2 then return message
-  if (num <= 2) {
-    return "Number must be greater than 2";
-  }
-
-  // Create for-loop to cycle through iterations based on the input number.
-  // add the value of B into the Array which should be initally 1..
-  // then set the inital value of C to equal the sum of A & B..
-  // then assign the value of B to A..
-  // Then Assign the value of C to B..
-  // Repeat loop which will increment the fibanacci sequence until the
-  // iterations based on the input number is complete.
-  for (let i = 0; i < num; i++) {
-    fibArr.push(b);
-    c = a + b;
-    a = b;
-    b = c;
-  }
-
-  // Return the result of the fibonacci sequence.
-  return fibArr;
-};
-
-//--------------------||✅ Final Test Results ✅||------------------------------
-// yarn run v1.22.19
-// warning package.json: No license field
-// $ /Users/learnacademy/Desktop/Repos/week-3-assessment-JeremyDuncan/
-// node_modules/.bin/jest
-//  PASS  ./code-challenges.test.js
-//   getFibonacci
-//     ✓ returns an array of numbers in Fibonacci sequence (1 ms)
-//     ✓ returns that number must be greater than 2
-//     ✓ returns that number must be greater than 2 if negative number
-
-// Test Suites: 1 passed, 1 total
-// Tests:       3 passed, 3 total
-// Snapshots:   0 total
-// Time:        0.18 s, estimated 1 s
-// Ran all test suites.
-// ✨  Done in 0.72s.
-//------------------------------------------------------------------------------
-
-//==============================================================================
-// --------------------2)👉 Create a function that takes in an object and
-//returns an array of the numbers sorted from least to greatest.
-//==============================================================================
-// a)🧪 Create a test with expect statements for each of the variables provided.
-//------------------------------------------------------------------------------
-
-describe("sortObjNumbers", () => {
-  it("returns an array of the numbers sorted from least to greatest.", () => {
-    //=== Provided Test Cases ===
-    const studyMinutesWeek1 = {
-      sunday: 90,
-      monday: 30,
-      tuesday: 20,
-      wednesday: 15,
-      thursday: 30,
-      friday: 15,
-      saturday: 60,
-    };
-    const studyMinutesWeek2 = {
-      sunday: 100,
-      monday: 10,
-      tuesday: 45,
-      wednesday: 60,
-      thursday: 20,
-      friday: 15,
-      saturday: 65,
-    };
-    //=== Expected Test Results ===
-    const expected1 = [15, 15, 20, 30, 30, 60, 90];
-    const expected2 = [10, 15, 20, 45, 60, 65, 100];
-
-    expect(sortObjNumbers(studyMinutesWeek1)).toEqual(expected1);
-    expect(sortObjNumbers(studyMinutesWeek2)).toEqual(expected2);
-  });
-});
-
-//---------------------||❌ Initial Test Results ❌||---------------------------
-// yarn run v1.22.19
-// warning package.json: No license field
-// $ /Users/learnacademy/Desktop/Repos/week-3-assessment-JeremyDuncan/
-// node_modules/.bin/jest
-//  FAIL  ./code-challenges.test.js
-//   getFibonacci
-//     ✓ returns an array of numbers in Fibonacci sequence (1 ms)
-//     ✓ returns that number must be greater than 2
-//     ✓ returns that number must be greater than 2 if negative number (2 ms)
-//   sortObjNumbers
-//     ✕ returns an array of the numbers sorted from least to greatest.
-
-//   ● sortObjNumbers › returns an array of the numbers sorted from least to
-//     greatest.
-
-//     ReferenceError: sortObjNumbers is not defined
-
-//       165 |     const expected2 = [10, 15, 20, 45, 60, 65, 100];
-//       166 |
-//     > 167 |     expect(sortObjNumbers(studyMinutesWeek1)).toEqual(expected1);
-//           |     ^
-//       168 |     expect(sortObjNumbers(studyMinutesWeek2)).toEqual(expected2);
-//       169 |   });
-//       170 | });
-
-//       at Object.expect (code-challenges.test.js:167:5)
-
-// Test Suites: 1 failed, 1 total
-// Tests:       1 failed, 3 passed, 4 total
-// Snapshots:   0 total
-// Time:        0.227 s, estimated 1 s
-// Ran all test suites.
-// error Command failed with exit code 1.
-// info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this
-// command.
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// b)👨‍💻 Create the function that makes the test pass.
-//------------------------------------------------------------------------------
-
-// declare function
-const sortObjNumbers = (obj) => {
-  // create array to hold sorted numbers
-  const numArr = [];
-  // loop through all object keys
-  for (const key in obj) {
-    // for each iteration push the value of the key into the array
-    numArr.push(obj[key]);
-  }
-  // return the value of the array that has been sorted from least to greatest
-  return numArr.sort(function (a, b) {
-    return a - b;
-  });
-};
-=======
 const studyMinutesWeek1 = {
   sunday: 90,
   monday: 30,
@@ -302,99 +48,13 @@ const studyMinutesWeek2 = {
   saturday: 65
 }
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
->>>>>>> parent of 3c30ea8 (updated formatting)
 
-//--------------------||✅ Final Test Results ✅||------------------------------
-// yarn run v1.22.19
-// warning package.json: No license field
-// $ /Users/learnacademy/Desktop/Repos/week-3-assessment-JeremyDuncan/
-// node_modules/.bin/jest
-//  PASS  ./code-challenges.test.js
-//   getFibonacci
-//     ✓ returns an array of numbers in Fibonacci sequence (1 ms)
-//     ✓ returns that number must be greater than 2
-//     ✓ returns that number must be greater than 2 if negative number
-//   sortObjNumbers
-//     ✓ returns an array of the numbers sorted from least to greatest.
+// b) Create the function that makes the test pass.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-// Test Suites: 1 passed, 1 total
-// Tests:       4 passed, 4 total
-// Snapshots:   0 total
-// Time:        0.217 s, estimated 1 s
-// Ran all test suites.
-// ✨  Done in 0.76s.
-//------------------------------------------------------------------------------
-=======
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
->>>>>>> parent of 3c30ea8 (updated formatting)
-=======
-// --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
->>>>>>> parent of 3c30ea8 (updated formatting)
-=======
-// --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
->>>>>>> parent of 3c30ea8 (updated formatting)
-=======
-// --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
->>>>>>> parent of 3c30ea8 (updated formatting)
 
-//==============================================================================
-// --------------------3)👉 Create a function that takes in an array and returns
-// an array of the accumulating sum. An empty array should return an empty array
-//==============================================================================
-// a)🧪 Create a test with expect statements for each of the variables provided.
-//------------------------------------------------------------------------------
-describe("accumulateArray", () => {
-  it("returns an array of the accumulating sum", () => {
-    //=== Provided Test Cases ===
-    const accountTransactions1 = [100, -17, -23, -9];
-    const accountTransactions2 = [250, -89, 100, -96];
-    //=== Expected Test Results ===
-    const expected1 = [100, 83, 60, 51];
-    const expected2 = [250, 161, 261, 165];
+// a) Create a test with expect statements for each of the variables provided.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    expect(accumulateArray(accountTransactions1)).toEqual(expected1);
-    expect(accumulateArray(accountTransactions2)).toEqual(expected2);
-  });
-  it("An empty array should return an empty array", () => {
-    //=== Provided Test Cases ===
-    const accountTransactions3 = [];
-    //=== Expected Test Results ===
-    const expected3 = [];
-
-    expect(accumulateArray(accountTransactions3)).toEqual(expected3);
-  });
-});
-
-//---------------------||❌  Initial Test Results ❌||--------------------------
-// yarn run v1.22.19
-// warning package.json: No license field
-// $ /Users/learnacademy/Desktop/Repos/week-3-assessment-JeremyDuncan/
-// node_modules/.bin/jest
-//  FAIL  ./code-challenges.test.js
-//   getFibonacci
-//     ✓ returns an array of numbers in Fibonacci sequence (1 ms)
-//     ✓ returns that number must be greater than 2
-//     ✓ returns that number must be greater than 2 if negative number (1 ms)
-//   sortObjNumbers
-//     ✓ returns an array of the numbers sorted from least to greatest.
-//   accumulateArray
-//     ✕ returns an array of the accumulating sum
-//     ✕ An empty array should return an empty array
-=======
-=======
->>>>>>> parent of 3c30ea8 (updated formatting)
-=======
->>>>>>> parent of 3c30ea8 (updated formatting)
-=======
->>>>>>> parent of 3c30ea8 (updated formatting)
 const accountTransactions1 = [100, -17, -23, -9]
 // Expected output: [100, 83, 60, 51]
 
@@ -403,91 +63,5 @@ const accountTransactions2 = [250, -89, 100, -96]
 
 const accountTransactions3 = []
 // Expected output: []
->>>>>>> parent of 3c30ea8 (updated formatting)
 
-//   ● accumulateArray › returns an array of the accumulating sum
-
-//     ReferenceError: accumulateArray is not defined
-
-//       263 |     const expected2 = [250, 161, 261, 165];
-//       264 |
-//     > 265 |     expect(accumulateArray(accountTransactions1)).toEqual(expected1);
-//           |     ^
-//       266 |     expect(accumulateArray(accountTransactions2)).toEqual(expected2);
-//       267 |   });
-//       268 |   it("An empty array should return an empty array", () => {
-
-//       at Object.expect (code-challenges.test.js:265:5)
-
-//   ● accumulateArray › An empty array should return an empty array
-
-//     ReferenceError: accumulateArray is not defined
-
-//       272 |     const expected3 = [];
-//       273 |
-//     > 274 |     expect(accumulateArray(accountTransactions3)).toEqual(expected3);
-//           |     ^
-//       275 |   });
-//       276 | });
-//       277 |
-
-//       at Object.expect (code-challenges.test.js:274:5)
-
-// Test Suites: 1 failed, 1 total
-// Tests:       2 failed, 4 passed, 6 total
-// Snapshots:   0 total
-// Time:        0.176 s, estimated 1 s
-// Ran all test suites.
-// error Command failed with exit code 1.
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// b)👨‍💻 Create the function that makes the test pass.
-//------------------------------------------------------------------------------
-
-// declare function
-const accumulateArray = (numArr) => {
-  // create variable with a value of zero to hold a value
-  // This is created outside the scope of the map() function so the data can be
-  // persistent across multiple iterations
-  let accumulator = 0;
-
-  //ternary operator:
-  // if input array is empty then "?" return an empty array..
-  // else ":" map through the array..
-  //  then store the value of the sum of "value" and accomulator
-  //  then set the accumulator value to the new value assigned previously
-  //  repeat the accumulation process till the array has been iterated through
-  return numArr.length === 0
-    ? []
-    : numArr.map((value) => {
-        value = value + accumulator;
-        accumulator = value;
-        return value;
-      });
-};
-
-//--------------------||✅ Final Test Results ✅||------------------------------
-// yarn run v1.22.19
-// warning package.json: No license field
-// $ /Users/learnacademy/Desktop/Repos/week-3-assessment-JeremyDuncan/
-// node_modules/.bin/jest
-//  PASS  ./code-challenges.test.js
-//   getFibonacci
-//     ✓ returns an array of numbers in Fibonacci sequence (1 ms)
-//     ✓ returns that number must be greater than 2
-//     ✓ returns that number must be greater than 2 if negative number
-//   sortObjNumbers
-//     ✓ returns an array of the numbers sorted from least to greatest. (1 ms)
-//   accumulateArray
-//     ✓ returns an array of the accumulating sum
-//     ✓ An empty array should return an empty array
-
-// Test Suites: 1 passed, 1 total
-// Tests:       6 passed, 6 total
-// Snapshots:   0 total
-// Time:        0.2 s, estimated 1 s
-// Ran all test suites.
-// ✨  Done in 0.78s.
-//------------------------------------------------------------------------------
-//==============================================================================
+// b) Create the function that makes the test pass.
--- interview-questions.md
+++ interview-questions.md
@@ -9,141 +9,39 @@ Then, research the question to expand on your answer. Even if you feel you have 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
 Your answer:
-* I like to think of JSX as the marriage betwwen HTML and JavaScript. JSX is 
- a language syntax used in React to create web applications.
- * In the return statement of a React component you need to use JSX fragments 
- ("<> </>") to encompase all the code you wish to be displayed in the DOM. These
- fragments can be empty, or contain other elements inside them such as a div or 
- even a table with nested data(as long as all the data is nested inside that 
- fragment)
- * Another intersting note about JSX is that it defaults to display HTML in the 
-component return, unless you use the {} to denote JavScript variables, and 
-functions, or React state.
-* One syntax difference is that with styles that use the "-" to denote 
-multi-word keys. In JSX you do not use "-" but instead combine words with 
-camelCase. 
-  * example: font-size = fontSize
-* Another syntax difference is you have to use "onClick" instead of "onclick" 
-for event listeners on mouse click events.
 
 Researched answer:
-* According to: https://reactjs.org/docs/introducing-jsx.html,
-it is a syntax extension of JavaScript which produce React elements. It also 
-mentions that JSX is not required to create React apps, but it is recomended.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
----
-<!-- ----------------------------------------------------------------------- -->
-2. What is yarn? What file(s) are modified in a React application when you run 
-yarn?
-=======
 2. What is yarn? What file(s) are modified in a React application when you run yarn?
->>>>>>> parent of 3c30ea8 (updated formatting)
-=======
-2. What is yarn? What file(s) are modified in a React application when you run yarn?
->>>>>>> parent of 3c30ea8 (updated formatting)
-=======
-2. What is yarn? What file(s) are modified in a React application when you run yarn?
->>>>>>> parent of 3c30ea8 (updated formatting)
-=======
-2. What is yarn? What file(s) are modified in a React application when you run yarn?
->>>>>>> parent of 3c30ea8 (updated formatting)
 
 Your answer:
-* Yarn is a package manager that allows developers to easily deploy, manage, 
-install, and keep uo to date on the latest versions of software and their 
-dependancies.
-* Yarn's main competitor is NPM, which has been around longer, offers developers 
-essentially the same functionality. The biggest difference being that Yarn is 
-faster and more secure than NPM. 
-* When you run run Yarn on a React application it checks your app directory and 
-verifies your applications dependencies. It will download any that are missing. 
 
 Researched answer:
-* According to: https://classic.yarnpkg.com/en/docs/getting-started,
-Yarn is a package manager for code that gives developers the ability to share 
-code with others. The code that's stored by Yarn is refered to as a Package or a
-Module. Yarn also uses a file called package.json to describe everything that is 
-in a particular package.
 
----
-<!-- ----------------------------------------------------------------------- -->
 3. What is an anonymous function in JavaScript?
 
 Your answer:
-* An anonymous function is a function that does not have a name. It can't be 
-called except for on the line of code it was created on. 
 
 Researched answer:
-* According to https://www.javascripttutorial.net/javascript-anonymous-functions/
-An anonymous function is a function that does not have a name.
 
-*Examples:
-```javascript
-let example = function() {
-  console.log("This is an anonymous function");
-}
-```
-```javascript
-(function() {
-  console.log("This is an immediatly invoked anonymous function");
-})();
-```
-```javascript
-let example = () => {console.log("This is a shortened version")};
-```
----
-<!-- ----------------------------------------------------------------------- -->
 4. What is the difference between state values and props in React?
 
 Your answer:
-* The difference between state values and props in React is that a state value 
-is created to represent data to be manipulated. Everytime a state value is 
-updated, React compares the difference of the Virtual DOM and actual DOM and 
-makes changes to only the affected nodes that changed. 
-* Props are used to access the value of state from other components. 
 
 Researched answer:
-* According to: https://www.geeksforgeeks.org/what-are-the-differences-between-props-and-state/#:~:text=Props%20are%20used%20to%20pass,be%20passed%20to%20other%20components.
-* State is the part of the application that changes. any component can have a 
-state, and the state is mutable.
-Props are properties that are used to pass data thats unmutable from one 
-component to another.  
----
-<!-- ----------------------------------------------------------------------- -->
+
 5. What is the DOM?
 
-Your answer: 
-* The DOM is known as the Document Object Model and is a visual 
-representaion of the code inside a webpage thats created by a web browser.
+Your answer:
 
 Researched answer:
-* According to: https://frontend.turing.edu/lessons/module-1/js-intro-to-the-dom.html?ads_cmpid=6451354298&ads_adid=76255849919&ads_matchtype=&ads_network=g&ads_creative=517671727591&utm_term=&ads_targetid=dsa-19959388920&utm_campaign=&utm_source=adwords&utm_medium=ppc&ttv=2&gclid=Cj0KCQjwsrWZBhC4ARIsAGGUJuq0hVzqEMjsv45Z-vMONxGC3orsxivCMoA2-xD94FqykOaqV7UJLEUaAr5iEALw_wcB,
-* The DOM is the interface for HTML. The DOM is created after the web browser 
-interpets the HTML code. When the DOM is created it produces a DOM tree thats 
-logically organized. The DOM can be accessed by using the document call.
 
-examples:
-```html
-<!-- Accessing the DOM with code -->
-document.getElementById("main-section")
-document.getElementsByTagName("a")[0]
-document.getElementsByTagName("button")
-```
----
-<!-- ----------------------------------------------------------------------- -->
 6. STRETCH: Which is the difference between a div and a span?
 
 Your answer:
 
-
 Researched answer:
 
----
-<!-- ----------------------------------------------------------------------- -->
 ## Looking Ahead: Terms for Next Week
 
 1. Object-oriented programming:
